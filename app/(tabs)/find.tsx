09e3ecedebdbc648347b09833387c43160cde2b2
Diego Federico Bravo <fedebravo2016@gmail.com>
2024-10-30 19:40:01 -0300

    agrego card para ver detalle en el mapa

--- app/(tabs)/find.tsx
+++ app/(tabs)/find.tsx
@@ -1,17 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  Animated,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useNavigation } from "expo-router";
 import { LA_PLATA_COORDENADAS } from "../../constants/Coordenadas";
-import { markers } from "../../assets/markers"; 
+import { markers } from "../../assets/markers";
 import CustomMarker from "@/components/CustomMarker";
 import MapViewDirections from "react-native-maps-directions";
+import Card from "@/components/Card";
+import Colors from "@/constants/Colors";
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = 220;
+const CARD_WIDTH = width * 0.8;
+const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
 
 export default function HomeScreen() {
   const [ubicacionUsuario, setUbicacionUsuario] = useState(null);
   const navigation = useNavigation();
+  const _map = useRef(null);
+  const _scrollView = useRef(null);
   const GOOGLE_MAPS_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_KEY;
+  let mapIndex = 0;
+  let mapAnimation = new Animated.Value(0);
+
+  useEffect(() => {
+    mapAnimation.addListener(({ value }) => {
+      let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
+      if (index >= markers.length) {
+        index = markers.length - 1;
+      }
+      if (index <= 0) {
+        index = 0;
+      }
+
+      clearTimeout(regionTimeout);
+
+      const regionTimeout = setTimeout(() => {
+        if (mapIndex !== index) {
+          mapIndex = index;
+          const { coordinate } = markers[index];
+          _map.current.animateToRegion(
+            {
+              ...coordinate,
+              latitudeDelta: LA_PLATA_COORDENADAS.latitudeDelta,
+              longitudeDelta: LA_PLATA_COORDENADAS.longitudeDelta,
+            },
+            350
+          );
+        }
+      }, 10);
+    });
+  });
+
+  const interpolations = markers.map((marker, index) => {
+    const inputRange = [
+      (index - 1) * CARD_WIDTH,
+      index * CARD_WIDTH,
+      ((index + 1) * CARD_WIDTH),
+    ];
+
+    const scale = mapAnimation.interpolate({
+      inputRange,
+      outputRange: [1, 2.5, 1],
+      extrapolate: "clamp",
+    });
+    return { scale };
+  });
+
 
   useEffect(() => {
     (async () => {
@@ -29,6 +96,7 @@ export default function HomeScreen() {
     <View style={styles.container}>
       {ubicacionUsuario && (
         <MapView
+          ref={_map}
           style={styles.map}
           initialRegion={{
             latitude: LA_PLATA_COORDENADAS.latitude,
@@ -52,34 +120,199 @@ export default function HomeScreen() {
             //   apikey={}
             // >
             // </MapViewDirections>
-            
+
           ))}
 
-        {markers.map((marker, index) => {
-          if (index < markers.length - 1) {
-            const nextMarker = markers[index + 1];
+          {markers.map((marker, index) => {
+            if (index < markers.length - 1) {
+              const nextMarker = markers[index + 1];
 
-            return (
-              <MapViewDirections
-                key={index}
-                origin={{ latitude: marker.latitude, longitude: marker.longitude }}
-                destination={{ latitude: nextMarker.latitude, longitude: nextMarker.longitude }}
-                apikey={GOOGLE_MAPS_KEY}
-                strokeWidth={5}
-                mode="WALKING"
-                strokeColor="black"
-                precision="low"
-              />
-            );
-          }
-        })}
+              return (
+                <MapViewDirections
+                  key={index}
+                  origin={{ latitude: marker.latitude, longitude: marker.longitude }}
+                  destination={{ latitude: nextMarker.latitude, longitude: nextMarker.longitude }}
+                  apikey={GOOGLE_MAPS_KEY}
+                  strokeWidth={5}
+                  mode="WALKING"
+                  strokeColor="black"
+                  precision="low"
+                />
+              );
+            }
+          })}
         </MapView>
       )}
-    </View>
+      <Animated.ScrollView
+        ref={_scrollView}
+        horizontal
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        style={styles.scrollView}
+        pagingEnabled
+        snapToInterval={CARD_WIDTH + 20}
+        snapToAlignment="center"
+        contentInset={{
+          top: 0,
+          left: SPACING_FOR_CARD_INSET,
+          bottom: 0,
+          right: SPACING_FOR_CARD_INSET,
+        }}
+        contentContainerStyle={{
+          paddingHorizontal: Platform.OS === "android" ? SPACING_FOR_CARD_INSET : 0,
+        }}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: mapAnimation,
+                }
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+      >
+        {
+          markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <Image
+                source={marker.image}
+                style={styles.cardImage}
+              />
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>{marker.title}</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>{marker.description}</Text>
+              </View>
+              <View style={styles.button}>
+                <TouchableOpacity
+                  style={[styles.textSign, {
+                    borderColor: Colors.text.primary,
+                    borderWidth: 1,
+                  }]}
+                // onPress={}   //aca iria para navegar a la pantalla de detalle de cada card
+                >
+                  <Text style={[styles.signIn, {
+                    color: Colors.text.primary,
+                  }]}>Ver más</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))
+        }
+      </Animated.ScrollView >
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
   map: { flex: 1 },
+  searchBox: {
+    position: 'absolute',
+    marginTop: Platform.OS === 'ios' ? 40 : 20,
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    width: '90%',
+    alignSelf: 'center',
+    borderRadius: 5,
+    padding: 10,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+  chipsScrollView: {
+    position: 'absolute',
+    top: Platform.OS === 'ios' ? 90 : 80,
+    paddingHorizontal: 10
+  },
+  chipsIcon: {
+    marginRight: 5,
+  },
+  chipsItem: {
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 8,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    height: 35,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    // padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  textContent: {
+    flex: 2,
+    padding: 10,
+  },
+  cardtitle: {
+    fontSize: 12,
+    // marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+  },
+  marker: {
+    width: 30,
+    height: 30,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 5
+  },
+  signIn: {
+    width: '100%',
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3
+  },
+  textSign: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
 });
