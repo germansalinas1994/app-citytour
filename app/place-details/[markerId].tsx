3044afd1387d2267cb9ed7d3eeefe3af779b85e4
Valentina Diaz <valendiaz@gmail.com>
2024-11-15 17:07:52 -0300

    se agrega boton de google maps y se cambia la visual de la pantalla detalle

--- app/place-details/[markerId].tsx
+++ app/place-details/[markerId].tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
+import { View, SafeAreaView, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Dimensions, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 import { markers } from '../../assets/markers';
@@ -10,15 +10,24 @@ import { useLocalSearchParams } from "expo-router";
 
 const screenWidth = Dimensions.get('window').width;
 
+
 export default function MarkerDetailsScreen() {
     const { markerId } = useLocalSearchParams();
-    const [urlToShow, setUrlToShow] = useState<string | null>(null); // Estado para almacenar el URL a mostrar en WebView
+    const [urlToShow, setUrlToShow] = useState<string | null>(null); 
 
     const selectedMarker = markers.find(marker => marker.id === parseInt(markerId as string));
 
     const speakDescription = () => {
-        if (selectedMarker && selectedMarker.description) {
+        if (selectedMarker?.description) {
             Speech.speak(selectedMarker.description, { language: 'es-ES' });
+        }
+    };
+
+    const openMap = (mapsUrl?: string) => {
+        if (mapsUrl) {
+            Linking.openURL(mapsUrl);
+        } else {
+            console.log("No se encontró un enlace de Google Maps.");
         }
     };
 
@@ -26,52 +35,82 @@ export default function MarkerDetailsScreen() {
         return <Text style={styles.noMarkerText}>No se encontró el marcador con el ID proporcionado.</Text>;
     }
 
-    // Renderiza WebView si hay un URL seleccionado
     if (urlToShow) {
-      console.log(urlToShow);
         return (
             <WebView
                 style={styles.webView}
                 source={{ uri: urlToShow }}
-                onError={() => setUrlToShow(null)} // Maneja errores de carga del URL
+                onError={() => setUrlToShow(null)}
             />
         );
     }
 
     return (
-        <ScrollView contentContainerStyle={styles.container}>
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.imageContainer}>
             <Image source={selectedMarker.image} style={styles.image} resizeMode="cover" />
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>{selectedMarker.title}</Text>
-                <TouchableOpacity onPress={speakDescription} style={styles.circleButton}>
-                    <Icon name="volume-up" size={20} color="#fff" />
-                    <Text style={styles.buttonText}>Escuchar</Text>
-                </TouchableOpacity>
+            
+            {/* Botón "Escuchar" en la esquina inferior izquierda */}
+            <TouchableOpacity onPress={speakDescription} style={styles.buttonOverlayRight}>
+                <Icon name="volume-up" size={25} color="#fff" />
+                {/* <Text style={styles.buttonText}>Escuchar</Text> */}
+            </TouchableOpacity>
+
+            {/* Botón "Mapas" al lado del otro botón */}
+            <TouchableOpacity onPress={() => openMap(selectedMarker.maps)} style={styles.buttonOverlay}>
+                <Icon name="map-marker" size={25} color="#fff" />
+                <Text style={styles.buttonText}>Ir</Text>
+            </TouchableOpacity>
+        </View>
+
+
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>{selectedMarker.title}</Text>
+                </View>
+
+                <Text style={styles.description}>{selectedMarker.description}</Text>
+
+                {selectedMarker.instagram && (
+                    <TouchableOpacity onPress={() => setUrlToShow(selectedMarker.instagram)} style={styles.linkContainer}>
+                        <Text style={styles.textlink}>Visite la página de </Text>
+                        <Text style={styles.link}>Instagram</Text>
+                    </TouchableOpacity>
+                )}
+
+                {selectedMarker.sitioOficial && (
+                    <TouchableOpacity onPress={() => setUrlToShow(selectedMarker.sitioOficial)} style={styles.linkContainer}>
+                        <Text style={styles.textlink}>Visite el </Text>
+                        <Text style={styles.link}>Sitio Oficial</Text>
+                    </TouchableOpacity>
+                )}
+            </ScrollView>
             </View>
-            <Text style={styles.description}>{selectedMarker.description}</Text>
-            {selectedMarker.instagram && (
-                <TouchableOpacity onPress={() => setUrlToShow(selectedMarker.instagram)} style={styles.linkContainer}>
-                    <Text style={styles.textlink}>Visite la página de </Text>
-                    <Text style={styles.link}>Instagram</Text>
-                </TouchableOpacity>
-            )}
-            {selectedMarker.sitioOficial && (
-                <TouchableOpacity onPress={() => setUrlToShow(selectedMarker.sitioOficial)} style={styles.linkContainer}>
-                    <Text style={styles.textlink}>Visite el </Text>
-                    <Text style={styles.link}>Sitio Oficial</Text>
-                </TouchableOpacity>
-            )}
-        </ScrollView>
+        </SafeAreaView>
+
     );
 }
-
 const styles = StyleSheet.create({
     container: {
+        width: '90%',  // Ajusta el ancho 
+        maxWidth: 400, // Limita el ancho máximo para que no se estire demasiado
+        padding: 15,
+        backgroundColor: Colors.varios.subicon,  
+        borderRadius: 15,  
+        shadowColor: '#000',  
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,  // opacidad de la sombra
+        shadowRadius: 4,
+        elevation: 5,  // Sombra para Android
+        overflow: 'hidden',  // Esto asegura que la imagen no se salga del contenedor con bordes redondeados
+      },
+      // Contenedor de scroll para contenido dinámico
+      scrollContainer: {
         flexGrow: 1,
-        padding: 20,
-        backgroundColor: Colors.varios.subicon,
-        alignItems: 'center',
-    },
+        justifyContent: 'center', // Alinea el contenido al centro
+        alignItems: 'center', // Alinea el contenido al centro
+      },
     webView: {
         flex: 1,
         marginTop: Constants.statusBarHeight,
@@ -80,11 +119,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 8,
-    },
-    image: {
-        width: screenWidth,
-        height: screenWidth * 0.6,
-        marginBottom: 16,
     },
     titleContainer: {
         flexDirection: 'row',
@@ -96,11 +130,11 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: 'bold',
+        marginTop: 15,
         marginBottom: 8,
         fontFamily: "outfit",
         color: 'black',
-        textAlign: 'left',
-        paddingLeft: 25,
+        marginLeft: 20,
     },
     description: {
         fontSize: 16,
@@ -108,46 +142,99 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         fontFamily: "outfit",
         color: 'black',
-        paddingLeft: 25,
-        paddingRight: 25,
+        paddingHorizontal: 25,
     },
     textlink: {
         fontSize: 16,
         textAlign: 'justify',
-        marginBottom: 15,
         fontFamily: "outfit",
         color: 'black',
-        paddingLeft: 25,
         lineHeight: 20,
     },
     link: {
         color: 'blue',
         textDecorationLine: 'underline',
-        marginBottom: 15,
-        lineHeight: 20,
         fontFamily: "outfit",
     },
     noMarkerText: {
         fontSize: 16,
-        color: 'white',
+        color: 'grey', // Mejor contraste si el fondo es claro
         textAlign: 'center',
         marginTop: 20,
         fontFamily: "outfit",
     },
-    circleButton: {
-        width: 50,
-        height: 50,
-        borderRadius: 30,
-        backgroundColor: Colors.varios.background,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 2,
-    },
+  
     buttonText: {
         color: 'white',
         textAlign: 'center',
-        fontSize: 6,
+        fontSize: 15, // Tamaño mejorado para visibilidad
         fontFamily: "outfit-Bold",
         marginLeft: 5,
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end', // Alinea los botones a la derecha
+        alignItems: 'center',
+        marginBottom: 10,
+        width: '100%', // Asegura que ocupe el ancho completo del contenedor
+        paddingRight: 5, // Añade un margen derecho para separar los botones del borde de la pantalla
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor: Colors.varios.background,
+        justifyContent: 'center', // Centrado vertical y horizontal
+        alignItems: 'center',
+    },
+    imageContainer: {
+        position: 'relative', // Necesario para posicionar los botones sobre la imagen
+        width: screenWidth,
+        height: screenWidth * 0.6,
+        marginBottom: 16,
+    },
+    
+    image: {
+        width: screenWidth,
+        height: screenWidth * 0.6,
+        marginBottom: 16,
+        borderRadius: 35,  // Si quieres bordes redondeados
+      },
+    buttonOverlay: {
+        position: 'absolute',
+        bottom: -25,
+        right: 40,    // Ajustado para que no se solapen
+        width: 55,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: Colors.varios.background,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+        flexDirection: 'row',
+        paddingHorizontal: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 5,
+      },
+    
+      buttonOverlayRight: {
+        position: 'absolute',
+        bottom: -25,
+        right: 105,    // Asegura que no se solapen con el primer botón
+        width: 55,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: Colors.varios.background,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+        flexDirection: 'row',
+        paddingHorizontal: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 5,
+      },
 });
--- assets/markers.ts
+++ assets/markers.ts
@@ -7,7 +7,8 @@ export const markers = [
         latitude: -34.914469395647906,
         longitude: -57.947065517191724,
         sitioOficial: "https://www.gba.gob.ar/casadegobierno",
-        linkDeInteres: "https://es.wikipedia.org/wiki/Casa_de_Gobierno_de_la_Provincia_de_Buenos_Aires"
+        linkDeInteres: "https://es.wikipedia.org/wiki/Casa_de_Gobierno_de_la_Provincia_de_Buenos_Aires",
+        maps: "https://www.google.com/maps/place/Casa+de+Gobierno+de+la+Provincia+de+Buenos+Aires/@-34.9145774,-57.9498333,17z/data=!3m1!4b1!4m6!3m5!1s0x95a2e63a3198d507:0xfcd8f573a1b24f7d!8m2!3d-34.9145774!4d-57.947253!16s%2Fg%2F122qy5pt?hl=es&entry=ttu&g_ep=EgoyMDI0MTExMi4wIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 2,
@@ -16,7 +17,8 @@ export const markers = [
         image: require('./images/markers/pasaje_rodrigo.jpg'),
         latitude: -34.91297511107016,
         longitude: -57.946918876534625,
-        instagram: "https://www.instagram.com/pasajerodrigo/?hl=es"
+        instagram: "https://www.instagram.com/pasajerodrigo/?hl=es",
+        maps: "https://www.google.com/maps/dir//Pasaje+Rodrigo,+Av.+51+491,+B1900+La+Plata,+Provincia+de+Buenos+Aires/@-34.9131335,-57.9494348,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e63979673947:0xa7140c61f7f97ed0!2m2!1d-57.9468545!2d-34.9131335!3e0?hl=es&entry=ttu&g_ep=EgoyMDI0MTExMi4wIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 3,
@@ -25,7 +27,8 @@ export const markers = [
         image: require('./images/markers/sede_gelp.jpg'),
         latitude: -34.91293732591926,
         longitude: -57.94541790537067,
-        sitioOficial: "https://www.gimnasia.org.ar/sedes/"
+        sitioOficial: "https://www.gimnasia.org.ar/sedes/",
+        maps: "https://www.google.com.ar/maps/dir//Club+de+Gimnasia+y+Esgrima+La+Plata,+Calle+4,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9130799,-57.9866284,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e63bfb010da1:0xe41fe2520d193706!2m2!1d-57.9453428!2d-34.9131573!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 4,
@@ -34,7 +37,8 @@ export const markers = [
         image: require('./images/markers/ministerio_seguridad.jpg'),
         latitude: -34.91149923030202,
         longitude: -57.943944866772604,
-        sitioOficial: "https://www.gba.gob.ar/seguridad"
+        sitioOficial: "https://www.gba.gob.ar/seguridad",
+        maps: "https://www.google.com.ar/maps/dir//Ministerio+de+Seguridad+De+La+Provincia+De+Buenos+Aires,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9107017,-57.9839973,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e63ea4cc6ae9:0x5609bed476000718!2m2!1d-57.9439663!2d-34.9116225!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 5,
@@ -42,7 +46,8 @@ export const markers = [
         description: "Lugar de encuentro familiar y recreación, con juegos infantiles, senderos y sectores de descanso, enmarcado por edificios históricos.",
         image: require('./images/markers/plaza_rivadavia.jpg'),
         latitude: -34.91055731727482,
-        longitude: -57.942824139763154
+        longitude: -57.942824139763154,
+        maps: "https://www.google.com.ar/maps/dir//Plaza+Rivadavia,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9107017,-57.9839973,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e63eeb4981bb:0xdedd93249a79ea58!2m2!1d-57.9427117!2d-34.9107791!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 6,
@@ -52,16 +57,18 @@ export const markers = [
         latitude: -34.91120153567593,
         longitude: -57.94174882444485,
         instagram: "https://www.instagram.com/casacurutchet.capba/",
-        sitioOficial: "https://es.wikiarquitectura.com/edificio/casa-curutchet/"
+        sitioOficial: "https://es.wikiarquitectura.com/edificio/casa-curutchet/",
+        maps: "https://www.google.com.ar/maps/dir//Casa+Curutchet,+Avenida+53,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9112738,-57.9830344,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e63e86517f97:0x15e61d9fed078f7e!2m2!1d-57.9417939!2d-34.9113125!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 7,
-        title: "Estadio UNO - Club Estudiantes de La Plata",
+        title: "Estadio UNO",
         description: "Conocido por su diseño moderno y tecnología avanzada. Ofrece un espacio para eventos deportivos y culturales de gran envergadura.",
         image: require('./images/markers/estadio.jpg'),
         latitude: -34.91180895002351,
         longitude: -57.9389311408224,
-        sitioOficial: "https://estudiantesdelaplata.com/"
+        sitioOficial: "https://estudiantesdelaplata.com/",
+        maps: "https://www.google.com.ar/maps/dir//Estadio+UNO+Jorge+Luis+Hirschi,+Av.+1+1150-1198,+B1900+La+Plata,+Provincia+de+Buenos+Aires/@-34.9117639,-57.9802575,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e61621b73869:0x32c529fe42daaa5d!2m2!1d-57.9389719!2d-34.9118413!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 8,
@@ -69,7 +76,8 @@ export const markers = [
         description: "Parte del Parque Pereyra Iraola, este lago es un destino natural popular para actividades recreativas al aire libre, como paseos en bote y picnics.",
         image: require('./images/markers/lago_bosque.jpg'),
         latitude: -34.910269715076815,
-        longitude: -57.938017388174345
+        longitude: -57.938017388174345,
+        maps: "https://www.google.com.ar/maps/dir//Lago+del+Bosque,+Avenida+Iraola,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9103947,-57.9793459,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e615e0895def:0xff9f0f263d28ee9c!2m2!1d-57.9380603!2d-34.9104721!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 9,
@@ -77,23 +85,26 @@ export const markers = [
         description: "Una avenida arbolada que cruza el Bosque de La Plata y es ideal para caminatas, ciclismo y actividades recreativas, rodeada de naturaleza y puntos de interés.",
         image: require('./images/markers/av_iraola.jpg'),
         latitude: -34.90953810444135,
-        longitude: -57.93530914584664
+        longitude: -57.93530914584664,
+        maps: "https://www.google.com.ar/maps/dir//Av.+Iraola,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9096785,-57.9456518,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e61427fee533:0x5692d41182f7d4d1!2m2!1d-57.9353306!2d-34.9096965!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 10,
         title: "Gruta",
-        description: "Ubicada en el Bosque de La Plata, la Gruta es un espacio natural y espiritual, popular para paseos y actividades recreativas en familia.",
+        description: "Ubicada en el Bosque de La Plata, es un espacio natural y espiritual, popular para paseos y actividades recreativas en familia.",
         //image: require('./images/markers/gruta.jpg'),
         latitude: -34.910309452003894,
-        longitude: -57.93591793482458
+        longitude: -57.93591793482458,
+        maps: "https://www.google.com.ar/maps/dir//Gruta+del+Bosque,+La+Plata,+Provincia+de+Buenos+Aires/@-34.910335,-57.9773092,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e72f38aa228f:0x957776b2725bf3a0!2m2!1d-57.9360236!2d-34.9104124!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 11,
         title: "Anfiteatro Martín Fierro",
-        description: "Este anfiteatro al aire libre alberga eventos culturales y artísticos, con una acústica destacada que ofrece una experiencia única para los espectadores.",
+        description: "Se encuentra aire libre, alberga eventos culturales y artísticos, con una acústica destacada que ofrece una experiencia única para los espectadores.",
         image: require('./images/markers/anfiteatro.jpg'),
         latitude: -34.91093869859924,
-        longitude: -57.935794166178475
+        longitude: -57.935794166178475,
+        maps: "https://www.google.com.ar/maps/dir//Anfiteatro+Mart%C3%ADn+Fierro,+Calle+Cuccolo,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9110449,-57.9773104,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x95a2e76e6748c549:0xf12ec02042626977!2m2!1d-57.9360248!2d-34.9111223!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 12,
@@ -101,16 +112,18 @@ export const markers = [
         description: "Un homenaje a los cinco sabios que diseñaron la ciudad de La Plata, representa la herencia cultural e histórica de la ciudad y su carácter educativo.",
         image: require('./images/markers/monumento.jpg'),
         latitude: -34.908877009947105,
-        longitude: -57.93659450351869
+        longitude: -57.93659450351869,
+        maps: "https://www.google.com.ar/maps/dir//Monumento+A+Los+Cinco+Sabios+De+La+Plata,+Avenida+Iraola,+La+Plata,+Provincia+de+Buenos+Aires/@-34.9089932,-57.9778801,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e615e7e2328d:0xfc514c7609534061!2m2!1d-57.9365945!2d-34.9090706!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     },
     {
         id: 13,
-        title: "Museo",
-        description: "El Museo de Ciencias Naturales de La Plata es uno de los más importantes de América Latina, con una vasta colección de fósiles y exposiciones sobre ciencias naturales e historia.",
+        title: "Museo de Ciencias Naturales",
+        description: "Es uno de los más importantes de América Latina, con una vasta colección de fósiles y exposiciones sobre ciencias naturales e historia.",
         image: require('./images/markers/museo.jpg'),
         latitude: -34.90875377900324,
         longitude: -57.935307043388924,
-        sitioOficial: "https://www.museo.fcnym.unlp.edu.ar/"
+        sitioOficial: "https://www.museo.fcnym.unlp.edu.ar/",
+        maps: "https://www.google.com.ar/maps/dir//Museo+de+Ciencias+Naturales+de+La+Plata,+Paseo+del+Bosque+s%2Fn,+B1900+La+Plata,+Provincia+de+Buenos+Aires/@-34.9088821,-57.9379119,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x95a2e6143077af85:0xa466b8a8cd326a29!2m2!1d-57.9353316!2d-34.9088865!3e0?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
     }
 ];
 
